e4f90347cb9ff278b47d117cbb282248478931bd
juseungjuseung <juseungjuseung@gmail.com>
2026-08-19 13:01:56 +0900

    feat: OPTIONS HTTP METHOD 허용으로 변경

--- src/main/java/com/hackathon/backend/config/SecurityConfig.java
+++ src/main/java/com/hackathon/backend/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.hackathon.backend.security.JwtAuthenticationEntryPoint;
 import com.hackathon.backend.security.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -46,6 +47,7 @@ public class SecurityConfig {
                                 "/v3/api-docs/**",
                                 "/h2-console/**"
                         ).permitAll()
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
